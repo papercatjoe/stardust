d5fb86c477244bd64ac0d1eb14a51771b9ab8e88
papercatjoe <papercatjoe@gmail.com>
2022-08-02 23:58:58 -0500

    Refactors config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
-export const apikey = process.env.NODE_ENV === 'test' ? process.env.STARDUST_APIKEY as string : ''
-
-export const gameId = 984
+export const nodeEnv = process.env.NODE_ENV
+export const isTestEnv = nodeEnv === 'test'
+export const apikey = isTestEnv ? process.env.STARDUST_APIKEY as string : ''
+export const gameId = (isTestEnv ? +(process.env.GAME_ID as string) : 0) || 0
